167d33b8e21cb4dc9934dae893d977857eb911e8
Nicola Lacerenza <nicolace98gmail.com>
2024-09-11 12:58:25 +0200

    UPDATE 11/09/2024 12:30

--- src/main/java/models/DettagliOrdine.java
+++ src/main/java/models/DettagliOrdine.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DettagliOrdine implements Oggetti<DettagliOrdine> {
-
     private int idProdotti;
     private int idOrdine;
     private int quantita;
