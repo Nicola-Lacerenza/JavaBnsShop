7354efa88d4eec2accaa809a8677858ee9ee4d56
antoniotassone94 <antonio.tassone@studio.unibo.it>
2024-09-28 17:43:34 +0200

    aggiornamenti antonio 28/09/2024

--- src/main/java/utility/GestioneToken.java
+++ src/main/java/utility/GestioneToken.java
@@ -1,22 +1,41 @@
 package utility;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.SignatureAlgorithm;
-
+import io.jsonwebtoken.SignatureAlgorithm;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
-
+import java.util.Optional;
 
 public class GestioneToken {
-
     private GestioneToken(){}
 
-    public String creaToken(){
-        String jwt = Jwts.builder()
-                .setSubject("user123")
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 ora
-                .signWith(SignatureAlgorithm.HS256, "secretKey")
+    public String createToken(String email){
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        long expMillis = nowMillis + 3600000;
+        byte[] apiKeySecretBytes = Base64.getEncoder().encode(("OXJX73LCZVhF5gdxJxDPhAPEdgnyK19i").getBytes());
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)
+                .expiration(new Date(expMillis)) // 1 ora
+                .signWith(signatureAlgorithm,signingKey)
                 .compact();
+    }
 
+    public static Optional<Claims> decodeJWT(String jwt) {
+        String[] chunks = jwt.split("\\.");
+        if(chunks.length < 2){
+            return Optional.empty();
+        }
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String header = new String(decoder.decode(chunks[0]));
+        String payload = new String(decoder.decode(chunks[1]));
+
+        return Optional.empty();
     }
 }
