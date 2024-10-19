bc831c977c8e201d8f42386c408ed9e21f776839
Nicola Lacerenza <nicolace98gmail.com>
2024-10-19 19:34:54 +0200

    UPDATE NL 19/10/2024 19:00

--- src/main/java/bnsshop/bnsshop/AuthServlet.java
+++ src/main/java/bnsshop/bnsshop/AuthServlet.java
@@ -41,6 +41,7 @@ public class AuthServlet extends HttpServlet{
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+
     }
 
     @Override
